b45a2016fe27ffdc4e8b953b8364269d1bd81b40
quantum <quantumworldtechnology@protom.me>
2025-05-30 21:20:36 +0530

    update course details

--- src/app/components/course-details/course-details.component.ts
+++ src/app/components/course-details/course-details.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CourseDataService } from '../../services/course-data.service';
 import { NgFor, NgIf } from '@angular/common';
 import {
   NgbAccordionModule,
   NgbCarouselModule,
 } from '@ng-bootstrap/ng-bootstrap';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-course-details',
@@ -12,11 +13,24 @@ import {
   templateUrl: './course-details.component.html',
   styleUrl: './course-details.component.css',
 })
-export class CourseDetailsComponent {
+export class CourseDetailsComponent implements OnInit {
   course: any = '';
-  constructor(private courseData: CourseDataService) {
-    this.courseData.getCourseData().subscribe((data) => {
-      this.course = data;
-    });
+  constructor(
+    private courseData: CourseDataService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const paramId = this.route.snapshot.paramMap.get('id');
+    const id = paramId ? String(paramId) : null;
+
+    if (id !== null) {
+      this.courseData.getCourseById(id).subscribe((data) => {
+        this.course = data;
+        console.log(data);
+      });
+    } else {
+      console.error('Invalid course ID in route.');
+    }
   }
 }
--- src/app/services/course-data.service.ts
+++ src/app/services/course-data.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +8,13 @@ import { Injectable } from '@angular/core';
 export class CourseDataService {
   constructor(private http: HttpClient) {}
 
-  getCourseData() {
-    return this.http.get('assets/data/course-data.json');
+  getCourseData(): Observable<any> {
+    return this.http.get<any[]>('assets/data/course-data.json');
+  }
+  // Get course by ID
+  getCourseById(id: string): Observable<any> {
+    return this.getCourseData().pipe(
+      map((courses: any[]) => courses.find((course) => course.id === id))
+    );
   }
 }
